ca74d3b7baffd555562bb7a3c1f01d8996b4f9e2
qpcio <qpcio@onet.eu>
2021-01-17 15:44:00 +0100

    zadanie domowe

---
+++ src/main/java/Day2/DoDomu/Garaz.java
@@ -0,0 +1,14 @@
+package Day2.DoDomu;
+
+public class Garaz {
+    /* Zaprojektujcie miks dziedziczenia i kompozycji
+    na przyklad: silnik -> silnik spalinowy ->  silnik diesla
+                                            -> silnik benzynowy
+                         -> silnik elektryczny
+                kola -> rozne kola itd.
+
+    z powyzszego zbudujcie samochod (vmax) -> toyota (160)
+                                            -> ferrari (280)
+
+     */
+}
--- src/main/java/Day2/Mody2/Klasa5.java
+++ src/main/java/Day2/Mody2/Klasa5.java
@@ -1,9 +1,11 @@
 package Day2.Mody2;
 
 import Day2.Mody1.Klasa1;
+import Day2.Mody1.Klasa2;
 
 public class Klasa5 {
-    private Klasa1 klasa1;
+    private Klasa1 klasa1;       // <- to jest kompozycja
+
     public void test(){
         klasa1 = new Klasa1();
 
